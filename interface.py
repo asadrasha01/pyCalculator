7975b7858fda2528d0d8283f3059f8be60621fe8
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2023-10-21 23:52:49 +0300

    Additional buttons

--- interface.py
+++ interface.py
@@ -21,6 +21,36 @@ multiply_btn()
 divide_btn = Button(window, text="/")
 divide_btn()
 
+zero_btn = Button(window, text="0")
+zero_btn()
+
+one_btn = Button(window, text="1")
+one_btn()
+
+two_btn = Button(window, text="2")
+two_btn()
+
+three_btn = Button(window, text="3")
+three_btn()
+
+four_btn = Button(window, text="4")
+four_btn()
+
+five_btn = Button(window, text="5")
+five_btn()
+
+six_btn = Button(window, text="6")
+six_btn()
+
+seven_btn = Button(window, text="7")
+seven_btn()
+
+eight_btn = Button(window, text="8")
+eight_btn()
+
+nine_btn = Button(window, text="9")
+nine_btn()
+
 display = Entry(window,justify=RIGHT)
 
 display.pack(side=TOP, expand=YES, fill=BOTH)
