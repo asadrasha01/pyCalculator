a506c54bf23eabd04032630d546a23c3a5899059
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2024-04-03 00:17:20 +0300

    everything working deleted useless files

--- main.py
+++ main.py
@@ -41,9 +41,27 @@ calculator_result.grid(columnspan=5)
 # Buttons
 for i in range(9):
     btn = tk.Button(root, text=str(i + 1), command=lambda x=i + 1: add_to_calculation(x), width=5, font=('Arial', 14))
-    btn.grid(row=i // 3 + 1, column=i % 3)
+    btn.grid(row=(i // 3 + 1)+1, column=i % 3)
+
+btn_plus = tk.Button(root, text='+', command=lambda: add_to_calculation('+'), width=5, font=('Arial', 14))
+btn_plus.grid(row=2, column=4)
+btn_minus = tk.Button(root, text='-', command=lambda: add_to_calculation('-'), width=5, font=('Arial', 14))
+btn_minus.grid(row=3, column=4)
+btn_mul = tk.Button(root, text='*', command=lambda: add_to_calculation('*'), width=5, font=('Arial', 14))
+btn_mul.grid(row=4, column=4)
+btn_div = tk.Button(root, text='/', command=lambda: add_to_calculation('/'), width=5, font=('Arial', 14))
+btn_div.grid(row=5, column=4)
+btn_open_p = tk.Button(root, text='(', command=lambda: add_to_calculation('('), width=5, font=('Arial', 14))
+btn_open_p.grid(row=5, column=0)
+btn_close_p = tk.Button(root, text=')', command=lambda: add_to_calculation(')'), width=5, font=('Arial', 14))
+btn_close_p.grid(row=5, column=2)
+btn_clear = tk.Button(root, text='C', command=clear_field, width=10, font=('Arial', 14))
+btn_clear.grid(row=6, column=0, columnspan=2)
+btn_equal = tk.Button(root, text='=', command=evaluate_calculation, width=10, font=('Arial', 14))
+btn_equal.grid(row=6, column=2, columnspan=2)
 
 btn0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, font=('Arial', 14))
-btn0.grid(row=4, column=0, columnspan=2)
+btn0.grid(row=5, column=1)
+
 
 root.mainloop()
